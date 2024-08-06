007a04dba5f0dd0244f8dd7767af04676a93c766
soumya <soumya.mukherjee0901@gmail.com>
2024-08-06 19:26:07 +0530

    NA-0: negative numbers test one failing when negative - is used as delimeter

--- sum.js
+++ sum.js
@@ -1,11 +1,18 @@
-module.exports  = function add(string) {
-    var regex = /\d+/g;
+function add(string) {
+    var regex = /[\d\-]+/g;
     var numbers = string.match(regex);
-    console.log(numbers);
     var total = 0;
     for(var i=0; i < numbers.length; i++) {
-        total += parseInt(numbers[i]);
+        var num = parseInt(numbers[i], 10);
+        console.log(num);
+        if (num >= 0) {
+            total += num;
+        }
     }
-
+    console.log("total: ",total);
     return total;
 }
+
+add("//;\n1;2, -3 ' ' ''5");
+
+module.exports = add;
--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,15 @@
 const add  = require('./sum');
 
 describe("sum", () => {
+    test('empty numbers/strings with actual integer addition', () => {
+        expect(add('1,"""" $2')).toBe(3);
+        expect(add('1," "  ""4 2')).toBe(7);
+        expect(add('1,""""4 is 2')).toBe(7);
+    });
+    test('adds numbers with negative integers as well in string', () => {
+        expect(add('1, 2, -9, --10, 5')).toBe(8);
+    });
     test('adds numbers in string', () => {
-        expect(add('1, 2')).toEqual(3);
+        expect(add('1, 2')).toBe(3);
     });
 });
