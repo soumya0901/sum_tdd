336d41c773a8909fd11263368f1923dc54046579
soumya <soumya.mukherjee0901@gmail.com>
2024-08-06 20:24:17 +0530

    Handle case for negative numbers to be displayed by coma and throwing an error

--- sum.js
+++ sum.js
@@ -3,11 +3,22 @@ function add(string) {
     var numbers = string.match(regex);
     var total = 0;
     console.log(numbers);
+    var negNum = [];
     for(var i=0; i < numbers.length; i++) {
         var num = parseInt(numbers[i], 10);
         if (num >= 0) {
             total += num;
+        } else {
+            negNum.push(num);
         }
+    }
+
+    try {
+        if(negNum.length) {
+            throw new Error(`negative numbers not allowed ${negNum.join(',')}`);
+        }
+    } catch (error) {
+        console.log(error.message);
     }
     console.log("total: ", total);
     return total;
--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,9 @@
 const add  = require('./sum');
 
 describe("sum", () => {
+    test('adds numbers in string', () => {
+        expect(add('1, 2')).toBe(3);
+    });
     test('empty numbers/strings with actual integer addition', () => {
         expect(add('1,"""" $2')).toBe(3);
         expect(add('1," "  ""4 2')).toBe(7);
@@ -18,7 +21,9 @@ describe("sum", () => {
     test('adds numbers with positive integers as well even when they act as delimiter in string', () => {
         expect(add('1, 2, -9, +++10^%$ +++++++5')).toBe(18);
     });
-    test('adds numbers in string', () => {
-        expect(add('1, 2')).toBe(3);
+    test('adds numbers with newline chars in string', () => {
+        expect(add('1\n2, -\n9,\n +++10^%$ +++++++5')).toBe(27);
     });
+
+    
 });
