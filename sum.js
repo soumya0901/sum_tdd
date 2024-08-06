9c81ef8f4aa9d6df99f4e877d6acd4258fb23b87
soumya <soumya.mukherjee0901@gmail.com>
2024-08-06 20:09:25 +0530

    Added fix for - operator when it acts as delimiter

--- sum.js
+++ sum.js
@@ -1,18 +1,18 @@
 function add(string) {
-    var regex = /[\d\-]+/g;
+    var regex = /(\d|(\-(?=\d)))+/g;
     var numbers = string.match(regex);
     var total = 0;
+    console.log(numbers);
     for(var i=0; i < numbers.length; i++) {
         var num = parseInt(numbers[i], 10);
-        console.log(num);
         if (num >= 0) {
             total += num;
         }
     }
-    console.log("total: ",total);
+    console.log("total: ", total);
     return total;
 }
 
-add("//;\n1;2, -3 ' ' ''5");
+add("//;\n1111;2,--3 \' \' ''5");
 
 module.exports = add;
